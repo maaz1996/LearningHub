512317d4df4ff598f9bc1cccae03a88827813398
maaz1996 <maaz.abdullah111@gmail.com>
2019-12-20 11:56:23 +0530

    updated files; deprecation warning resolved

--- index.js
+++ index.js
@@ -3,9 +3,13 @@ const morgan = require("morgan");
 const bodyParser = require("body-parser");
 const app = express();
 const mongoose = require("mongoose");
+mongoose.set("useCreateIndex", true);
 const config = require("./config/index");
 
-db = mongoose.connect("mongodb+srv://root:password@123@dev-i9qmj.mongodb.net/mydb?retryWrites=true&w=majority", { useNewUrlParser: true });
+db = mongoose.connect(
+  "mongodb+srv://root:password@123@dev-i9qmj.mongodb.net/mydb?retryWrites=true&w=majority",
+  { useNewUrlParser: true }
+);
 
 //middleware
 app.use(morgan("dev"));
@@ -13,9 +17,12 @@ app.use(bodyParser.json());
 
 //CORS Headers
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
 });
 
 // User Routes
@@ -26,10 +33,6 @@ app.use("/user", userRoute);
 const adminAuthRoutes = require("./routes/admin/auth/adminAuth");
 app.use("/admin", adminAuthRoutes);
 
-//odata leavemanagement
-const odatamanagement = require("./routes/odata/leaveManagement/getleave");
-app.use("/odata", odatamanagement);
-
 // Admin Routes
 const adminActionsRoutes = require("./routes/admin/actions/adminAction");
 app.use("/admin/action", adminActionsRoutes);
@@ -38,11 +41,6 @@ app.use("/admin/action", adminActionsRoutes);
 const personalRoutes = require("./routes/user/action/user");
 app.use("/personal", personalRoutes);
 
-//admin personal route 
-const adminpersonalRoutes = require("./routes/admin/actions/adminaction");
-app.use("/personal", adminpersonalRoutes);
-
-
 app.listen(config["port"], () => {
-    console.log(`Server listening on port: ${config["port"]}`);
+  console.log(`Server listening on port: ${config["port"]}`);
 });
